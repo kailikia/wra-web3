52c032768d3331f300ea41d52bbf28c939c4162e
kailikia <edwinkailikia@gmail.com>
2022-01-26 10:49:27 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+## Frontend Routes
+
+@app.route("/")
+def home():
+    return render_template("frontend/index.html")
+
+@app.route("/about-us")
+def about():
+    return render_template("frontend/about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("frontend/projects.html")
+
+@app.route("/contact-us")
+def contact():
+    return render_template("frontend/contact.html")
+
+@app.route("/tenders")
+def tenders():
+    return render_template("tenders.html")
+
+@app.route("/publications")
+def publications():
+    return render_template("publications.html")
+
+## End Frontend Routes
+
+## Backend Routes
+
+
+
+## Backend Roues
+
+
+if __name__ == '__main__':
+    app.run()
